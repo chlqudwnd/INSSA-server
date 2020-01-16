e9916c9421d27c63981cd49d08367c83581c212e
chlqudwnd <chio6622@gmail>
2020-01-16 12:19:03 +0900

    user, club 분기 나눔

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import basicRouter from './route/basicRouter';
+import userRouter from './route/user';
+import clubRouter from './route/club';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser());
 app.use('/someurl', basicRouter);
+
+app.use('/user', userRouter);
+app.use('/club', clubRouter);
 
 export default app;
