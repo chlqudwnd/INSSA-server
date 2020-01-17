b12b52d3b9c262739b2ca845e2e9de62f8afb4ba
chlqudwnd <chio6622@gmail>
2020-01-17 20:03:20 +0900

    create users api

--- src/route/users.ts
+++ src/route/users.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import mypageRouter from './mypage';
-import { userInfo } from 'os';
 
 const router = express.Router();
 // 1. 유저가 post 요청 (email, username, password)
@@ -8,63 +6,67 @@ const router = express.Router();
 // 3. true => 응답 (409, conflic) false => 응답 (200, save)
 
 // signup
-router.post('/', (req, res) => {
-  //crypto로 암호화 모듈파일 생성
-  const { id, password, username, phone, address, gender, birth, type } = req.body;
+// router.post('/', (req, res) => {
+//crypto로 암호화 모듈파일 생성
+// const { id, password, username, phone, address, gender, birth, type } = req.body;
 
-  user
-    .findOrCreate({
-      where: {
-        id: id,
-      },
-      defaults: {
-        id: id,
-        password: password,
-        username: username,
-        phone: phone,
-        address: address,
-        gender: gender,
-        birth: birth,
-        type: type,
-      },
-    })
-    .spread((user, created) => {
-      if (created) {
-        res.status(200).send('ok');
-      } else {
-        res.status(409).send('conflic');
-      }
-    });
-});
+//   user
+//     .findOrCreate({
+//       where: {
+//         id: id,
+//       },
+//       defaults: {
+//         id: id,
+//         password: password,
+//         username: username,
+//         phone: phone,
+//         address: address,
+//         gender: gender,
+//         birth: birth,
+//         type: type,
+//       },
+//     })
+//     .spread((user, created) => {
+//       if (created) {
+//         res.status(200).send('ok');
+//       } else {
+//         res.status(409).send('conflic');
+//       }
+//     });
+// });
 
-//가입된 모든 회원 보기 요청
-router.get('/', (req, res) => {
-  user.findAll().then(result => {
-    if (result.length) {
-      res.status(200).send(result);
-    } else {
-      res.status(404).send('not found');
-    }
-  });
-});
+// //가입된 모든 회원 보기 요청
+// router.get('/', (req, res) => {
+//   user.findAll().then(result => {
+//     if (result.length) {
+//       res.status(200).send(result);
+//     } else {
+//       res.status(404).send('not found');
+//     }
+//   });
+// });
 
-//회원 탈퇴 요청
-router.delete('/', (req, res) => {
-  const userId = req.session.userid;
-  user
-    .destroy({
-      where: {
-        id: userId,
-      },
-    })
-    .then(result => {
-      if (result === 1) {
-        res.status(200).send('삭제됨');
-      } else {
-        res.status(404).send('');
-      }
-    });
-});
+// // 회원 정보 수정 요청
+
+// router.patch('/', (req, res) => {});
+
+// //회원 탈퇴 요청
+// router.delete('/', (req, res) => {
+//   const userId = req.session.userid;
+//   user
+//     .destroy({
+//       where: {
+//         id: userId,
+//       },
+//     })
+//     .then(result => {
+//       if (result === 1) {
+//         res.status(200).send('삭제됨');
+//       } else {
+//         res.status(404).send('');
+//       }
+//     });
+// });
 
 // 1. 유저가 로그인 POST 요청 (email, password)
 
