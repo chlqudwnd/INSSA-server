85bb88a9869879cd60edb1eafcf60eaa666902da
valentine256 <icarmenjm@gmail.com>
2020-01-17 18:00:11 +0900

    add entities (Comment, Session)

---
+++ src/data/entity/Comment.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryColumn, CreateDateColumn, UpdateDateColumn, OneToOne, JoinColumn } from 'typeorm';
+import { User } from './User';
+@Entity()
+export class Session {
+  @PrimaryColumn()
+  id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToOne(
+    type => User,
+    user => user.id,
+    {
+      cascade: true,
+      nullable: false,
+    },
+  )
+  @JoinColumn()
+  user: User;
+}
