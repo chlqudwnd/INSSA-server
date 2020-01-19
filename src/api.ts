fee68c73a163766f25906b179f363d2f4d790165
chlqudwnd <chio6622@gmail>
2020-01-19 11:50:51 +0900

    board post요청

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,12 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieparser from 'cookie-parser';
 import basicRouter from './route/basicRouter';
-// import userRouter from './route/users';
+import userRouter from './route/users';
 import clubRouter from './route/clubs';
-// import sessionRouter from './route/sessions';
-// import commentRouter from './route/comments';
-// import boardRouter from './route/boards';
+import sessionRouter from './route/sessions';
+import commentRouter from './route/comments';
+import boardRouter from './route/boards';
+import hobbysRouter from './route/hobbys';
 
 const api = express();
 
@@ -21,12 +22,13 @@ api.get('/', (req, res) => {
 });
 api.use('/api', basicRouter);
 
-// api.use('/users', userRouter);
-// api.use('/clubs', clubRouter);
-// api.use('/boards', boardRouter);
-// api.use('/comments', commentRouter);
-// api.use('/sessions', sessionRouter);
+api.use('/users', userRouter);
+api.use('/clubs', clubRouter);
+api.use('/boards', boardRouter);
+api.use('/comments', commentRouter);
+api.use('/sessions', sessionRouter);
 // api.use('/meetings');
+api.use('/hobbys', hobbysRouter);
 // api.use('/mypage');
 
 export default api;
