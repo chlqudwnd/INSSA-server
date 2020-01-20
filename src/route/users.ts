7afd553675cf9ead9c9bc7f0d440e5bd2c929593
chlqudwnd <chio6622@gmail>
2020-01-20 21:25:53 +0900

    add check token

--- src/route/users.ts
+++ src/route/users.ts
@@ -1,101 +1,102 @@
 import express from 'express';
 import { User } from '../data/entity/User';
-import jwt from 'jsonwebtoken';
-import secret from '../config/jwt';
+import { decryptId } from '../config/decryptId';
+import { encrypt } from '../config/encrypt';
+import { Hobby } from '../data/entity/Hobby';
 const router = express.Router();
-// 1. 유저가 post 요청 (email, username, password)
-// 2. 요청이 온 email을 database에 있는지 확인
-// 3. true => 응답 (409, conflic) false => 응답 (200, save)
 
-// signup
-router.post('/', async (req, res) => {
-  // crypto로 암호화 모듈파일 생성
-  const { id, password, name, phone, address, gender, birth, type } = req.body;
-  const find = await User.findOne({ id: id });
-  const user =
-    !find &&
-    (await User.create({
-      id: id,
-      password: password,
-      name: name,
-      phone: phone,
-      address: address,
-      gender: gender,
-      birth: birth,
-      type: 'nomal',
-    })
-      .save()
-      .catch((err: Error) => {
-        res.send('번호 정확히 입력');
-      }));
-
-  if (user) {
-    res.send('회원 가입 완료');
+router.get('*', (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    next();
   } else {
-    res.send('중복된 아이디 입니다');
+    res.status(400).send('토큰 만료 다시 로그인 해주세요');
   }
 });
 
-// //가입된 모든 회원 보기 요청
-router.get('/', (req, res) => {
-  req;
-  User.find().then((result: { length: any }) => {
-    if (result.length) {
-      res.status(200).send(result);
-    } else {
-      res.status(404).send('not found');
+// signup
+router.post('/', async (req, res) => {
+  const { id, password, name, phone, address, gender, birth, type, hobbys } = req.body;
+  const finduser = await User.findOne({ id: id });
+  if (finduser) {
+    res.status(409).send('아이디 중복');
+    return;
+  }
+  const newPassword = await encrypt(password);
+  const newUser = new User();
+  newUser.id = id;
+  newUser.password = newPassword;
+  newUser.name = name;
+  newUser.phone = phone;
+  newUser.address = address;
+  newUser.gender = gender;
+  newUser.birth = birth;
+  newUser.type = 'true';
+  await User.save(newUser);
+  const user = await User.findOne({ where: { id: id }, relations: ['hobbys'] });
+  hobbys.map(async (id: number) => {
+    const info = await Hobby.findOne({ id: id });
+    if (user && info) {
+      user.hobbys.push(info);
+      User.save(user);
     }
   });
+  res.status(201).send(`환영합니다 ${id} 님!`);
+});
+
+// //나의 정보 보기 요청
+router.get('/', async (req, res) => {
+  const userId = await decryptId(req);
+  if (!userId) {
+    res.send('잘못된 요청입니다');
+  }
+  const users = await User.find({ id: userId });
+  const resData =
+    users.length &&
+    users.map(item => {
+      const { id, name, address, gender, phone } = item;
+      return {
+        id,
+        name,
+        address,
+        gender,
+        phone,
+      };
+    });
+  res.status(200).send(resData);
 });
 
 // // 회원 정보 수정 요청
 
-router.patch('/', (req, res) => {
-  const token = req.cookies.token;
-  const body = req.body;
-  const { id } = body;
-  if (token) {
-    User.update(
-      {
-        id: id,
-      },
-      body,
-    )
-      .then((result: any) => {
-        res.status(201).send(result);
-      })
-      .catch((err: Error) => {
-        res.status(400).send(err);
-      });
+router.patch('/', async (req, res) => {
+  const { name, phone, address } = req.body;
+  const userId = await decryptId(req);
+  const findUser = await User.findOne({ phone: phone });
+  console.log('finduser', findUser);
+  const user = !findUser && (await User.update({ id: userId }, { name: name, phone: phone, address: address }));
+  console.log('user', user);
+  if (user) {
+    res.status(201).send(user);
   } else {
-    res.status(400).send('잘못된 요청');
+    res.status(409).send('휴대폰 번호를 정확히 입력하세요');
   }
 });
 
 //회원 탈퇴 요청
 router.delete('/', async (req, res) => {
-  const token = req.cookies.token;
-  const user = Object.values(jwt.verify(token, secret))[0];
-  console.log('name', user);
-  if (user) {
-    User.update(
+  const userId = await decryptId(req);
+  const deleteUser =
+    userId &&
+    (await User.update(
+      { id: userId },
       {
-        id: user,
+        /*status:"d"*/
       },
-      {
-        // status: 'd',
-      },
-    )
-      .then((result: any) => {
-        res.clearCookie('token');
-        console.log('result', result);
-        res.status(201).send('bye bye');
-      })
-      .catch((err: Error) => {
-        res.status(401).send(err);
-      });
+    ));
+  if (deleteUser.affected) {
+    res.status(200).send('탈퇴');
   } else {
-    res.status(400).send('잘못된 요청입니다');
+    res.status(404).send('not found');
   }
 });
 
